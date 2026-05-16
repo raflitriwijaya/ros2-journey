504b286eafc80da856ecf5e0a4ceaa9199a258bf
raflitriwijaya <triwijaya.rafli@gmail.com>
2026-05-16 12:21:26 +0700

    P2L7 - custom message publish

--- irrigation_robot/src/lesson7_custom_msg_pub.cpp
+++ irrigation_robot/src/lesson7_custom_msg_pub.cpp
@@ -1,31 +1,55 @@
 #include "rclcpp/rclcpp.hpp"
-#include "irrigation_msgs/msg/soil_data.hpp"
+#include "irrigation_msgs/msg/soil_data.hpp"  // header pesan custom
 
 class SoilDataPublisher : public rclcpp::Node
 {
-private:
-    void timer_callback()
-    {
-        auto msg = irrigation_msgs::msgs::msg::SoilData();
-        msg.moisture_percent    = 42.5 + (count_ % 10) // fluctuates 42.5-51.5
-        msg.temperature_celcius = 28.3
-        msg.field_name          = "Field A"
-        msg.pump_active         = (msg.moisture_percent < 45.0) // pump active if moisture is below 45%
-
-        publisher_->publish(msg);
-
-        RC
-        
-    }
 public:
-    SoilDataPublisher : public rclcpp::Node(/* args */);
-    ~SoilDataPublisher : public rclcpp::Node();
+  SoilDataPublisher()
+  : Node("soil_data_publisher"), count_(0)
+  {
+    // Publisher dengan tipe message yang benar
+    publisher_ = this->create_publisher<irrigation_msgs::msg::SoilData>("soil_data", 10);
+
+    // Timer setiap 1 detik
+    timer_ = this->create_wall_timer(
+      std::chrono::seconds(1),
+      std::bind(&SoilDataPublisher::timer_callback, this));
+
+    RCLCPP_INFO(this->get_logger(), "Soil Data Publisher has been started.");
+  }
+
+private:
+  void timer_callback()
+  {
+    auto msg = irrigation_msgs::msg::SoilData();   // namespace yang benar
+    msg.moisture_percent = 42.5 + (count_ % 10);    // 42.5 - 51.5
+    msg.temperature_celsius = 28.3;                 // typo diperbaiki, titik koma ditambah
+    msg.field_name = "Field A";
+    msg.pump_active = (msg.moisture_percent < 45.0); // aktif jika moisture < 45%
+    msg.timestamp = this->now();                     // builtin_interfaces/Time
+
+    publisher_->publish(msg);
+
+    RCLCPP_INFO(this->get_logger(),
+      "[%s] moisture: %.2f%%, temperature: %.2fC, pump_active: %s",
+      msg.field_name.c_str(),
+      msg.moisture_percent,
+      msg.temperature_celsius,
+      msg.pump_active ? "ON" : "OFF");
+
+    ++count_;
+  }
+
+  // Deklarasi anggota kelas
+  rclcpp::Publisher<irrigation_msgs::msg::SoilData>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  int count_;
 };
 
-SoilDataPublisher : public rclcpp::Node::SoilDataPublisher : public rclcpp::Node(/* args */)
+int main(int argc, char * argv[])
 {
-}
-
-SoilDataPublisher : public rclcpp::Node::~SoilDataPublisher : public rclcpp::Node()
-{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<SoilDataPublisher>());
+  rclcpp::shutdown();
+  return 0;
 }
